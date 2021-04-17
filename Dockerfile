90de99446b3be56331ebccb3ac6debfb6dd8c1e8
Jakub Ruchel <jruchel254@gmail.com>
2021-04-17 17:58:58 +0200

    Fixed problems with docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-FROM python:3.8-slim-buster
-
-WORKDIR /app
-
-EXPOSE 8080
-
+FROM python:3.7-alpine
+WORKDIR /code
+ENV FLASK_APP=app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+RUN apk add --no-cache gcc musl-dev linux-headers
 COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
-
+RUN pip install -r requirements.txt
+EXPOSE 5000
 COPY . .
-
-CMD [ "python3", "-m" , "flask", "run", "--host=localhost"]
+CMD ["flask", "run"]
--- app.py
+++ app.py
@@ -1,28 +1,35 @@
 import json
 from flask import request, Flask
+from flask_cors import CORS
 
 api = Flask(__name__)
 api.config["DEBUG"] = True
+CORS(api)
 
 
 @api.route('/files', methods=['GET'])
 def get_files():
-    database = open('database.txt', 'r')
-    files = []
-    for line in database.readlines():
-        files.append(json.loads(line))
+    try:
+        database = open('database.txt', 'r')
+        files = []
+        for line in database.readlines():
+            files.append(json.loads(line))
 
-    return create_response(files, 200)
+        return create_response(files, 200)
+    except Exception as e:
+        return create_response(e, 500)
 
 
 @api.route('/files', methods=['POST'])
 def post_file():
-    content = request.get_json()
-    database = open('database.txt', 'w+')
-    database.write(content)
-    database.close()
-
-    return create_response('Dodano', 200)
+    try:
+        content = request.get_json()
+        database = open('database.txt', 'w+')
+        database.write(content)
+        database.close()
+        return create_response('Dodano', 200)
+    except Exception as e:
+        return create_response(e, 500)
 
 
 def create_response(data, status):
@@ -30,4 +37,4 @@ def create_response(data, status):
 
 
 if __name__ == "__main__":
-    api.run(host='localhost', port=8080)
+    api.run(host='0.0.0.0')
